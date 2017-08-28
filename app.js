48cbc472305cd203f11039cb9ce234b4bf246ba6
Sam Steele <samsteele@User-Mac.local>
2017-08-28 15:21:24 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const path = require('path');
+const mustacheExpress = require('mustache-express');
+const app = express();
+const port = 3000;
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const mongoURL = 'mongodb://localhost:27017/newdb';
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/', function (req, res) {
+  MongoClient.connect(mongoURL, function (err, db) {
+    const robots = db.collection('robots');
+    robots.find({}).toArray(function (err, docs) {
+      res.render("index", {robots: docs})
+    })
+  })
+})
+
+app.get('/index/:id', function(req, res){
+  MongoClient.connect(mongoURL, function (err, db) {
+    const robots = db.collection('robots');
+    let id = parseInt(req.params.id);
+    robots.find({"id": id}).toArray(function (err, docs) {
+      console.log(docs);
+      res.render("robot", {robots: docs})
+    })
+  })
+})
+
+app.get('/looking', function (req, res) {
+  console.log("looking");
+  MongoClient.connect(mongoURL, function (err, db) {
+    const robots = db.collection('robots');
+    robots.find({"job": null}).toArray(function (err, docs) {
+      console.log(docs);
+      res.render("index", {robots: docs})
+    })
+  })
+})
+
+app.get('/employed', function (req, res) {
+  console.log("employed");
+  MongoClient.connect(mongoURL, function (err, db) {
+    const robots = db.collection('robots');
+    robots.find({"job": {$not: {$in: [null]}}}).toArray(function (err, docs) {
+      res.render("index", {robots: docs})
+    })
+  })
+})
+
+app.listen(port, function() {
+  console.log('Example listening on port 3000')
+})
